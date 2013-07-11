93a5f2533b683fd1ef99c0f732b3386c8bd39e5b
Dave Hodder <dave.hodder@focusrite.com>
2013-07-11 10:18:05 -0400

    midification commences, with apologies to webaudiodemos.appspot.com

---
+++ public/js/midi.js
@@ -0,0 +1,201 @@
+var theUniverse = null;
+var frame1 = null,
+    frame2 = null,
+	currentFrame = null,
+	backFrame = null;
+
+var numRows = 8,
+	numCols = 8;
+
+window.addEventListener('keydown', function() { tick();
+} );
+
+window.addEventListener('load', function() {
+	theUniverse = document.getElementById("universe");
+	frame1 = new Array(numRows);
+	frame2 = new Array(numRows);
+
+	for (var i=0; i<numRows; i++) {
+		frame1[i] = new Array(numCols);
+		frame2[i] = new Array(numCols);
+		for (var j=0; j<numCols; j++) {
+			frame1[i][j] = (Math.random() < 0.5);
+			frame2[i][j] = false;
+		}	
+	}
+
+	for (var i=0; i<numRows; i++) {
+		var rowElem = document.createElement("div");
+		rowElem.className = "row";
+		rowElem.row = i;
+		for (var j=0; j<numCols; j++) {
+			var cellElem = document.createElement("div");
+			cellElem.row = i;
+			cellElem.col = j;
+			cellElem.onclick = flipHandler;
+			cellElem.className = "cell";
+            
+            cellElem.xval = j;
+            cellElem.yval = i;
+    
+			if (frame1[i][j])
+				cellElem.classList.add("live");
+			rowElem.appendChild(cellElem);
+		}
+		theUniverse.appendChild(rowElem);
+	}
+	currentFrame = frame1;
+	backFrame = frame2;
+	navigator.requestMIDIAccess().then( onMIDIInit );
+} );
+
+var selectMIDIIn = null;
+var selectMIDIOut = null;
+var midiAccess = null;
+var midiIn = null;
+var midiOut = null;
+var launchpadFound = false;
+
+function changeMIDIIn( ev ) {
+  var list=midiAccess.inputs();
+  var selectedIndex = ev.target.selectedIndex;
+
+  if (list.length >= selectedIndex) {
+    midiIn = list[selectedIndex];
+    midiIn.onmidimessage = midiMessageReceived;
+  }
+}
+
+function changeMIDIOut( ev ) {
+  var list=midiAccess.outputs();
+  var selectedIndex = ev.target.selectedIndex;
+
+  if (list.length >= selectedIndex) {
+    midiOut = list[selectedIndex];
+	midiOut.send( [0xB0,0x00,0x00] ); // Reset Launchpad
+	midiOut.send( [0xB0,0x00,0x01] ); // Select XY mode
+	drawFullBoardToMIDI();
+  }
+}
+
+function onMIDIInit( midi ) {
+  var preferredIndex = 0;
+  midiAccess = midi;
+  selectMIDIIn=document.getElementById("midiIn");
+  selectMIDIOut=document.getElementById("midiOut");
+
+  var list=midiAccess.inputs();
+
+  // clear the MIDI input select
+  selectMIDIIn.options.length = 0;
+
+  for (var i=0; i<list.length; i++)
+    if (list[i].name.toString().indexOf("Launchpad") != -1) {
+      preferredIndex = i;
+      launchpadFound = true;
+    }
+
+  if (list.length) {
+    for (var i=0; i<list.length; i++)
+      selectMIDIIn.options[i]=new Option(list[i].name,list[i].fingerprint,i==preferredIndex,i==preferredIndex);
+
+    midiIn = list[preferredIndex];
+    midiIn.onmidimessage = midiProc;
+
+    selectMIDIIn.onchange = changeMIDIIn;
+  }
+
+  // clear the MIDI output select
+  selectMIDIOut.options.length = 0;
+  preferredIndex = 0;
+  list=midi.outputs();
+
+  for (var i=0; i<list.length; i++)
+    if (list[i].name.toString().indexOf("Launchpad") != -1)
+      preferredIndex = i;
+
+  if (list.length) {
+    for (var i=0; i<list.length; i++)
+      selectMIDIOut.options[i]=new Option(list[i].name,list[i].fingerprint,i==preferredIndex,i==preferredIndex);
+
+    midiOut = list[preferredIndex];
+    selectMIDIOut.onchange = changeMIDIOut;
+  }
+
+  if (midiOut && launchpadFound) {  
+	midiOut.send( [0xB0,0x00,0x00] ); // Reset Launchpad
+	midiOut.send( [0xB0,0x00,0x01] ); // Select XY mode
+	drawFullBoardToMIDI();
+  }
+}
+
+function flipHandler(e) {
+	flip( e.target );
+}
+
+function flip(elem) {
+	currentFrame[elem.row][elem.col] = !currentFrame[elem.row][elem.col];
+	if (elem.className == "cell")  // dead
+		elem.className = "cell live";
+	else
+		elem.className = "cell";
+	var key = elem.row*16 + elem.col;
+    
+    playClip(elem.xval, elem.yval);
+    
+	midiOut.send( [0x90, key, elem.classList.contains("live") ? (elem.classList.contains("mature")?0x13:0x30) : 0x00]);
+}
+
+function findElemByXY( x, y ) {
+	var e, i, j, c;
+
+	for (i in theUniverse.children) {
+		e = theUniverse.children[i];
+		if (e.row == y) {
+			for (j in e.children) {
+				if (e.children[j].col == x)
+					return e.children[j];
+			}
+		}
+	}
+	return null;
+}
+
+function flipXY( x, y ) {
+	var elem = findElemByXY( x, y );
+	if (elem)
+		flip( elem );
+}
+
+function drawFullBoardToMIDI() {
+	for (var i=0; i<numRows; i++) {
+		for (var j=0; j<numCols; j++) {
+			var key = i*16 + j;
+			midiOut.send( [0x90, key, currentFrame[i][j] ? (findElemByXY(j,i).classList.contains("mature")?0x13:0x30) : 0x00]);
+		}	
+	}
+}
+
+function midiProc(event) {
+  data = event.data;
+  var cmd = data[0] >> 4;
+  var channel = data[0] & 0xf;
+  var noteNumber = data[1];
+  var velocity = data[2];
+
+  if ( cmd==8 || ((cmd==9)&&(velocity==0)) ) { // with MIDI, note on with velocity zero is the same as note off
+    // note off
+    //noteOff(b);
+  } else if (cmd == 9) {  // Note on
+    if ((noteNumber&0x0f)==8)
+      tick();
+    else {
+      var x = noteNumber & 0x0f;
+      var y = (noteNumber & 0xf0) >> 4;
+      flipXY( x, y );
+    }
+  } else if (cmd == 11) { // Continuous Controller message
+    switch (b) {
+    }
+  }
+}
