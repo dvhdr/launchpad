036367cdd5ce12b1c6bb263478303c8218fba696
Dave Hodder <dave.hodder@focusrite.com>
2013-07-15 16:56:17 -0400

    putting in some of sandbox.js, without success.  Improved audio layout.

--- public/js/audio.js
+++ public/js/audio.js
@@ -1,30 +1,145 @@
 
-    // These are the things you need:  an API key, the track ID, and the path to the track
-    var apiKey = 'PGCZJJWKLITVGDF9Q';
-    var trackID = 'TRCYWPQ139279B3308';
-    var trackURL = 'audio/test.mp3';
-    
-    // Set up the key variables
-    var remixer;
-    var player;
-    var track;
+// These are the things you need:  an API key, the track ID, and the path to the track
+var apiKey = 'PGCZJJWKLITVGDF9Q';
+var trackID = 'TRCYWPQ139279B3308';
+var trackURL = 'audio/test.mp3';
 
-// The main function.
-function init() 
-{
-    // Make sure the browser supports Web Audio.
-    if (window.webkitAudioContext === undefined) 
-    {
-        $("#info").text("Web Audio is missing :(");
+// Set up the key variables
+var remixer;
+var player;
+var track;
+
+
+// Get an estimation of analysis time
+function fetchQinfo() {
+    var url = 'http://remix.echonest.com/Uploader/qinfo?callback=?'
+    $.getJSON(url, {}, function(data) {
+        $("#info").text("Estimated analysis time: " + Math.floor(data.estimated_wait * 1.2) + " seconds.");
+    });
+}
+
+// Get the analysis, if it is ready
+function analyzeAudio(audio, tag, callback) {
+    var url = 'http://remix.echonest.com/Uploader/qanalyze?callback=?'
+    $.getJSON(url, { url:audio, api_key:apiKey, tag:tag}, function(data) {
+        if (data.status === 'done' || data.status === 'error') {
+            callback(data);
+        } else {
+            $("#info").text(data.status + ' - ready in about ' + data.estimated_wait + ' secs. ');
+            setTimeout(function() { analyzeAudio(audio, tag, callback); }, 8000);
+        } 
+    });
+}
+
+function initUpload() {
+    if (window.webkitAudioContext === undefined) {
+        error("Sorry, this app needs advanced web audio. Your browser doesn't"
+            + " support it. Try the latest version of Chrome");
+    }
+    // Read the URL query string to decide what to do
+    var params = {};
+    var q = document.URL.split('?')[1];
+    if(q != undefined){
+        q = q.split('&');
+        for(var i = 0; i < q.length; i++){
+            var pv = q[i].split('=');
+            var p = pv[0];
+            var v = pv[1];
+            params[p] = v;
+        }
+    }
+
+    if ('key' in params) {
+        // We just uploaded a track.
+        // We need to log the trackID and the URL, and then redirect.
+        $("#select-track").hide();
+        $("#play-remix").hide();
+        $("#info").text("Analyzing audio...");
+        trackURL = 'http://' + params['bucket'] + '/' + urldecode(params['key']);
+
+        analyzeAudio(trackURL, 'tag', function(data) {
+            if (data.status === 'done') {
+                var newUrl = location.protocol + "//" +  location.host + location.pathname + "?trid=" + data.trid;
+                location.href = newUrl;
+            }
+        });
     } 
-    else 
-    {    
-    
-        // These set up the WebAudio playback environment, and create the remixer and player.
-        var context = new webkitAudioContext();
-        remixer = createJRemixer(context, $, apiKey);
-        player = remixer.getPlayer();
-        
+
+    else if ('trid' in params) {
+        // We were passed a trackID directly in the url
+        // We can remix the track we get back!
+        trackID = params['trid'];
+        $("#play-remix").show();
+        $("#select-track").hide();
+
+        var urlXHR = getProfile(trackID, function(data) {
+            trackURL = data.url;
+
+            if (data.status == true) {
+                console.log("Ready to remix");
+                var context = new webkitAudioContext();
+                // Only use the filesystem if we have access to it.
+                if (window.File && window.FileReader && window.FileList && window.Blob && window.webkitRequestFileSystem) {
+                    window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
+                    window.requestFileSystem(window.TEMPORARY, 1024*1024, function(filesystem) {
+                        fs = filesystem;
+                    }, fileErrorHandler);
+                }
+
+
+                remixer = createJRemixer(context, $, apiKey);
+                player = remixer.getPlayer();
+
+
+                loadRemixData();
+            }
+            else {
+                console.log("Track id error.");
+                $("#play-remix").hide();
+                $("#select-track").show();
+                $("#info").text("Error getting the track URL - please try again, or re-upload the file.");
+                $("#redirect-url").attr('value', document.URL);
+
+                $("#file").change( 
+                    function() {
+                        fetchQinfo();
+                        var filename = $("#file").val();
+                        if (endsWith(filename.toLowerCase(), ".mp3")) {
+                            $("#f-filename").attr('value', fixFileName(filename));
+                            $("#upload").removeAttr('disabled');
+                        } else {
+                            alert('Sorry, this app only supports MP3s');
+                            $("#upload").attr('disabled', 'disabled');
+                        }
+                    }
+                );
+                fetchSignature();
+            }
+        });
+    } else {
+        // We're waiting for the user to pick a track and upload it
+        $("#play-remix").hide();
+        $("#redirect-url").attr('value', document.URL);
+
+        $("#file").change( 
+            function() {
+                var filename = $("#file").val();
+                if (endsWith(filename.toLowerCase(), ".mp3")) {
+                    $("#f-filename").attr('value', fixFileName(filename));
+                    $("#upload").removeAttr('disabled');
+                } else {
+                    alert('Sorry, this app only supports MP3s');
+                    $("#upload").attr('disabled', 'disabled');
+                }
+            }
+        );
+        fetchSignature();
+        fetchQinfo();
+    }
+}
+
+function loadRemixData()
+{
          player.addAfterPlayCallback(playLoop);
     
         $("#info").text("Loading analysis data...");
@@ -53,11 +168,29 @@ function init()
             }
             
         });
+}
+
+// The main function.
+function initSimple() 
+{
+    // Make sure the browser supports Web Audio.
+    if (window.webkitAudioContext === undefined) 
+    {
+        $("#info").text("Web Audio is missing :(");
+    } 
+    else 
+    {    
+        // These set up the WebAudio playback environment, and create the remixer and player.
+        var context = new webkitAudioContext();
+        remixer = createJRemixer(context, $, apiKey);
+        player = remixer.getPlayer();
+        
+        loadRemixData();
     }
 }
 
 // Run the main function once the page is loaded.
-window.onload = init;
+window.onload = initSimple;
 
 //////////////////////////////////////////////////////////////
 // looping "engine"
@@ -105,40 +238,27 @@ function stopLooping()
 
 function playClip(x, y) 
 {
-    var yoff = y * 32;
+    var yoff = y * 64;
 
-    if (x < 4)
+    var xoff = x * 8;
+    
+    var nextLoop = xoff + yoff;
+    
+    var start = false;
+    if (lastLoop < 0 || lastLoop == nextLoop) 
     {
-        var xoff = x * 4;
-        
-        var start = false;
-        if (lastLoop < 0)
-        {
-            player.stop();
-            start = true;
-        }
-        lastLoop = yoff + xoff;    
-        
-        if (start)
-            playLoop();
+       stopLooping();
+        start = true;
     }
-    else
-    {
-        xoff = (x-4) * 4;
-        
-        var bar = new Array();
-        for (var i=0; i < 16; ++i)
-        {
-            bar.push(track.analysis.tatums[yoff + xoff]);
-        }
-        
-        stopLooping();
-        player.play(0, bar);
-    }
+    lastLoop = nextLoop;    
+    
+    if (start)
+        playLoop();
+
 }
 
 function playOriginal()
 {
     stopLooping();
-    player.play(0, track.analysis.beats)
+    player.play(0, track.analysis.sections)
 }
